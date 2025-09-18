d9337a988f8c8900071489899e2843e6e93f8487
LautyT02 <lautyt02@gmail.com>
2025-09-18 12:13:14 -0300

    Added POST request to the Router and enabled reading of recived json data in srever.ts

--- src/router.ts
+++ src/router.ts
@@ -7,4 +7,8 @@ router.get("/",(req,res)=>{
 router.get("/test1",(req,res)=>{
     res.send("Prueba 1")
 })
+//Auth
+router.post("/auth/register",(req,res)=>{
+    console.log(req.body)
+})
 export {router}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import {router} from "./router"
 const app = express()
+//Enable reading of json data
+app.use(express.json())
 //Routing
 app.use("/",router)
 
