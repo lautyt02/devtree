9d47336074a5c52afb2aba8a581cbcb3bd368924
LautyT02 <lautyt02@gmail.com>
2025-09-25 19:33:16 -0300

    Connected to mongoDB using mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import 'dotenv/config'
 import {router} from "./router"
+import { connectDB } from "./config/db"
 const app = express()
+connectDB()
 //Enable reading of json data
 app.use(express.json())
 //Routing
