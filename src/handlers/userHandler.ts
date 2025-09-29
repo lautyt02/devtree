2b358c63eab5ad6fb4e358faee7dce15c17ffb8b
LautyT02 <lautyt02@gmail.com>
2025-09-29 17:11:44 -0300

    Added handle to createUser function

--- src/handlers/userHandler.ts
+++ src/handlers/userHandler.ts
@@ -1,19 +1,27 @@
 import type { Request, Response } from "express"
+import slug from 'slug'
 import { User } from "../models/User"
 import { hashPassword } from "../utils/auth"
 
 
 export const createUser = async (req: Request, res: Response) => {
     const { email, password } = req.body
-    const userExists = !! await User.findOne({ email })
-    if (userExists) {
-        const error = new Error("El usuario ya existe")
+    const handle = slug(req.body.handle, "")
+
+    const emailExists = !! await User.findOne({ email })
+    const handleExists = !! await User.findOne({ handle })
+    if (emailExists) {
+        const error = new Error("El email ya está en uso")
         return res.status(409).json({ error: error.message })
     }
-    else {
-        const user = new User(req.body)
-        user.password = await hashPassword(password)
-        await user.save()
-        res.status(201).send("Usuario registrado correctamente")
+    if (handleExists) {
+        const error = new Error("El nombre de usuario ya está en uso")
+        return res.status(409).json({ error: error.message })
     }
+    const user = new User(req.body)
+    user.password = await hashPassword(password)
+    user.handle = handle
+    await user.save()
+    res.status(201).send(`Usuario registrado correctamente. Nombre de usuario: ${handle}`)
+
 }
