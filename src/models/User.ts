ed63f433777e4d756e4dd77e9f4cbff800196cf3
LautyT02 <lautyt02@gmail.com>
2025-09-29 16:52:04 -0300

    Added lowercase: true to email in User schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -20,7 +20,8 @@ const userSchema = new Schema({
         type: String,
         require: true,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
