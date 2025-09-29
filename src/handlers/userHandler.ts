fcd96b70f52c2cde4e5f6b8c680234a79b8d3e95
LautyT02 <lautyt02@gmail.com>
2025-09-29 16:46:59 -0300

    made use of hashPassword function in createUser function

--- src/handlers/userHandler.ts
+++ src/handlers/userHandler.ts
@@ -1,18 +1,19 @@
 import type { Request, Response } from "express"
 import { User } from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 
 export const createUser = async (req: Request, res: Response) => {
-    const { email } = req.body
+    const { email, password } = req.body
     const userExists = !! await User.findOne({ email })
     if (userExists) {
         const error = new Error("El usuario ya existe")
         return res.status(409).json({ error: error.message })
     }
     else {
-        await User.create(req.body)
+        const user = new User(req.body)
+        user.password = await hashPassword(password)
+        await user.save()
         res.status(201).send("Usuario registrado correctamente")
     }
-
-
 }
