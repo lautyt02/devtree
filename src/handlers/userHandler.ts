63156e74d21cef02f6c9f7c7c5b1fbcf25a19132
LautyT02 <lautyt02@gmail.com>
2025-09-30 10:48:20 -0300

    Created function to login users

--- src/handlers/userHandler.ts
+++ src/handlers/userHandler.ts
@@ -2,22 +2,22 @@ import type { Request, Response } from "express"
 import { validationResult } from "express-validator"
 import slug from 'slug'
 import { User } from "../models/User"
-import { hashPassword } from "../utils/auth"
+import { checkPasword, hashPassword } from "../utils/auth"
 
 
-export const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response) => {
     const errors = validationResult(req)
     const { email, password } = req.body
     const handle = slug(req.body.handle, "")
 
     const emailExists = !! await User.findOne({ email })
     const handleExists = !! await User.findOne({ handle })
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
     }
     if (emailExists) {
         const error = new Error("El email ya está en uso")
-        return res.status(409).json({ errors: error.message })
+        return res.status(409).json({ error: error.message })
     }
     if (handleExists) {
         const error = new Error("El nombre de usuario ya está en uso")
@@ -30,3 +30,27 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(201).send(`Usuario registrado correctamente. Nombre de usuario: ${handle}`)
 
 }
+
+const login = async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { userId, password } = req.body
+    console.log(userId)
+    const user = await User.findOne({ $or: [{ email: userId }, { handle: userId }] })
+    console.log(user)
+    if (!user) {
+        const error = new Error("No existe usuario con ese email o handle")
+        return res.status(404).json({ error: error.message })
+    }
+    const isPasswordCorrect = await checkPasword(password, user.password)
+    if (!isPasswordCorrect){
+        const error = new Error("Contraseña incorrecta")
+        return res.status(401).json({ error: error.message })
+    }
+    res.status(200).send("Login exitoso")
+}
+
+export { createUser, login }
