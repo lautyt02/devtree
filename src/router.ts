54c85f4257cb77681817066ffa952dfbf856134e
LautyT02 <lautyt02@gmail.com>
2025-09-30 11:00:30 -0300

    Moved handleling of input errors from userHendler.ts to its own middleware in middlewares/validation.ts and implemented it in router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { body } from 'express-validator'
 import { createUser, login } from "./handlers/userHandler";
+import { handleInputErrors } from "./middlewares/validation";
 const router = Router()
 //gets
 router.get("/", (req, res) => {
@@ -14,11 +15,13 @@ router.post("/auth/register",
     body('handle').notEmpty().withMessage("El handle no puede estar vacio"),
     body("name").notEmpty().withMessage("El nombre no puede estar vacio"),
     body("email").isEmail().withMessage("Email inválido"),
-    body("password").isLength({ min: 8 }).withMessage("La contraseña debe tener un mínimo de 8 caracteres")
-    , createUser)
+    body("password").isLength({ min: 8 }).withMessage("La contraseña debe tener un mínimo de 8 caracteres"),
+    handleInputErrors,
+    createUser)
 
 router.post("/auth/login",
     body("userId").notEmpty().withMessage("Ingrese su email o su handle"),
     body("password").notEmpty().withMessage("Ingrese una contraseña"),
+    handleInputErrors,
     login)
 export { router }
