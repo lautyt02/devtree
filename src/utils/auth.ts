07f177c2ed02044f706a25e5c31a44bcb9c7af45
LautyT02 <lautyt02@gmail.com>
2025-09-30 10:47:02 -0300

    Created function to check password

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,11 @@
 import bcrypt from "bcrypt"
 
-export const hashPassword = async (password:string)=>{
+const hashPassword = async (password: string) => {
     const salt = await bcrypt.genSalt(10)
-    return await bcrypt.hash(password,salt)
+    return await bcrypt.hash(password, salt)
 }
+
+const checkPasword = async (password: string, hash: string) => {
+    return await bcrypt.compare(password,hash)
+}
+export {hashPassword,checkPasword}
