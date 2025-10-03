5d0e415f044d1770d097dc722d6056fb1a6a28b1
LautyT02 <lautyt02@gmail.com>
2025-10-03 10:53:45 -0300

    Added Routing using React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Autentication } from './layouts'
+import { Home, Login, Register } from './pages'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <h1 className='font-bold text-red'>Devtree Frontend</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route element={<Autentication />}>
+            <Route path='/auth/login/' element={<Login/>}/>
+            <Route path='/auth/register/' element={<Register/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
